769b8c0782b4fbb03f041460b11d39c3e6955e71
Leonardo <leonardo.santos@convergenceworks.com.br>
2014-12-17 14:11:33 -0200

    removido o SANTOS

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 		<input type="submit" value="Calcular">
 		</p>
 	</form>
-	<span>Calculadora desenvolvida por Leonardo Santos</span>
+	<span>Calculadora desenvolvida por Leonardo</span>
 
 </body>
 </html>
